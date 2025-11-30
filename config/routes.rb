7e099e376c640694ef00f605af12e0b849bb182a
Nick Coyne <nick@codevader.com>
2025-12-01 07:41:35 +1300

    Remove namespace from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 AppStatus::Engine.routes.draw do
-  get '/index(.:format)', to: 'app_status/status#index'
-  root to: 'app_status/status#index'
+  get '/index(.:format)', to: 'status#index'
+  root to: 'status#index'
 end
